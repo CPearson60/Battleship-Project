d07482cbf69c23468b8060a06f44bade4dce52bf
giannacastr <62898067+giannacastr@users.noreply.github.com>
2024-07-08 12:15:15 -0400

    formatting

--- BattleShipOfficalCode.py
+++ BattleShipOfficalCode.py
@@ -4,15 +4,16 @@ from colorama import init, Fore, Style
 def battleGrid(displayGrid):
     
     # prints the grid
-    print(Fore.LIGHTCYAN_EX + f"\n{displayGrid[0][0]} | {displayGrid[0][1]} | {displayGrid[0][2]} | {displayGrid[0][3]} | {displayGrid[0][4]}")
-    print("----+-----+-----+-----+----")
-    print(Fore.LIGHTCYAN_EX + f"{displayGrid[1][0]} | {displayGrid[1][1]} | {displayGrid[1][2]} | {displayGrid[1][3]} | {displayGrid[1][4]}")
-    print("----+-----+-----+-----+----")
-    print(Fore.LIGHTCYAN_EX + f"{displayGrid[2][0]} | {displayGrid[2][1]} | {displayGrid[2][2]} | {displayGrid[2][3]} | {displayGrid[2][4]}")
-    print("----+-----+-----+-----+----")
-    print(Fore.LIGHTCYAN_EX + f"{displayGrid[3][0]} | {displayGrid[3][1]} | {displayGrid[3][2]} | {displayGrid[3][3]} | {displayGrid[3][4]}")
-    print("----+-----+-----+-----+----")
-    print(Fore.LIGHTCYAN_EX + f"{displayGrid[4][0]} | {displayGrid[4][1]} | {displayGrid[4][2]} | {displayGrid[4][3]} | {displayGrid[4][4]}\n")
+    print("\n   1     2     3     4     5")
+    print("A " + Fore.LIGHTCYAN_EX + f"{displayGrid[0][0]} | {displayGrid[0][1]} | {displayGrid[0][2]} | {displayGrid[0][3]} | {displayGrid[0][4]}")
+    print("  ----+-----+-----+-----+----")
+    print(Style.RESET_ALL + "B " + Fore.LIGHTCYAN_EX + f"{displayGrid[1][0]} | {displayGrid[1][1]} | {displayGrid[1][2]} | {displayGrid[1][3]} | {displayGrid[1][4]}")
+    print("  ----+-----+-----+-----+----")
+    print(Style.RESET_ALL + "C " + Fore.LIGHTCYAN_EX + f"{displayGrid[2][0]} | {displayGrid[2][1]} | {displayGrid[2][2]} | {displayGrid[2][3]} | {displayGrid[2][4]}")
+    print("  ----+-----+-----+-----+----")
+    print(Style.RESET_ALL + "D " + Fore.LIGHTCYAN_EX + f"{displayGrid[3][0]} | {displayGrid[3][1]} | {displayGrid[3][2]} | {displayGrid[3][3]} | {displayGrid[3][4]}")
+    print("  ----+-----+-----+-----+----")
+    print(Style.RESET_ALL + "E " + Fore.LIGHTCYAN_EX + f"{displayGrid[4][0]} | {displayGrid[4][1]} | {displayGrid[4][2]} | {displayGrid[4][3]} | {displayGrid[4][4]}\n")
     print(Style.RESET_ALL)
 
     return coordGrid, displayGrid
@@ -41,10 +42,11 @@ def gridUpdate(coordGrid, displayGrid):
 
 def player_shoot():
     
-    shot = input("Where do you want to shoot? [column][row]: ").strip()
+    shot = input("Where do you want to shoot? (X,#): ").strip()
+    shot = shot.replace(",","")
     valid = len(shot) == 2 and shot[0].lower() in ["a", "b", "c", "d","e"] and shot[1] in ["1", "2", "3", "4", "5"]
     while not valid:
-        shot = input("Wrong format, input [column],[row]: ").strip()
+        shot = input("Invalid input. Please enter a valid coordinate (X,#): ").strip()
         valid = len(shot) == 2 and shot[0].lower() in ["a", "b", "c", "d","e"] and shot[1] in ["1", "2", "3", "4", "5"]
     row_convert = {"a":0, "b":1, "c":2, "d":3, "e":4}
     row = row_convert[shot[0].lower()]
@@ -83,8 +85,9 @@ def random_num():
 def ship_point(ship_point_grid):
     
     ship_point_grid[random_num][random_num] = 1
-    print(ship_point_grid)
+    #print(ship_point_grid)
     
+print("Welcome to Battleship!") 
 random_num=random_num()
 
 ship_point(coordGrid)
