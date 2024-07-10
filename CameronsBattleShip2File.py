495c7f807b435d122ab3792bd1ca4d51ddc1cfba
CPearson60 <cameronlpearson2005@gmail.com>
2024-07-10 10:48:02 -0400

    Update CameronsBattleShip2File.py

--- CameronsBattleShip2File.py
+++ CameronsBattleShip2File.py
@@ -39,3 +39,21 @@ def player_win():
     print("                      /___________\ ")
     
 player_win()
+
+
+print(f"""{Fore.RED}Description:
+{Fore.LIGHTBLACK_EX}    The game starts with a 5x5 grid displayed on the screen.
+{Fore.LIGHTBLACK_EX}    You will be prompted to input the coordinates where you want to shoot.
+{Fore.LIGHTBLACK_EX}    Coordinates should be in the format X,# (e.g., A,1).
+{Fore.LIGHTBLACK_EX}    If your shot hits the ship, you win and the game ends.
+{Fore.LIGHTBLACK_EX}    If your shot misses, you can try again.
+{Fore.LIGHTBLACK_EX}    The game will update the grid after each shot to reflect the shots fired.\n""")
+
+print(Fore.WHITE+"Create Your Grid:")
+col_list = int(input(Fore.BLUE+"    How many columns (max 26): "))
+while col_list >= 27:
+    col_list = int(input("  Too many. Maximum columns is 26. How many columns: "))
+
+row_list = int(input("  How many rows (max 26): "))
+while row_list >= 27:
+    row_list = int(input("  Too many. Maximum rows is 26. How many rows: "))
