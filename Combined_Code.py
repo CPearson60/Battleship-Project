36abe2921e138742a1e9c721847aa716a0d46041
CPearson60 <cameronlpearson2005@gmail.com>
2024-07-17 10:26:08 -0400

    Update Combined_Code.py

--- Combined_Code.py
+++ Combined_Code.py
@@ -582,52 +582,69 @@ def Shoot_Guess(computerDisplayGrid, computerCoordGrid, col_list, row_list,turn)
 #////////////////////////////////////////////////(User Turn and Computer Turn Functions)\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 # Gianna
 # Function for computer to place ships
-def player_turn(computerDisplayGrid, computerCoordGrid, row1, col1, row2, col2, row3, col3, row4, col4, random_num3, random_num4, row_list, col_list):
-    
-    print("User Objective:\nSink The Computer's Ship")
-    
-    computerBattleGrid(computerDisplayGrid, row_list, col_list)
-    turn = 1
-    Shoot_Guess(computerDisplayGrid, computerCoordGrid, col_list, row_list,turn)
-    if computerCoordGrid[random_num3][random_num4] != "x":
-        shipAnimation_miss()
 
-    # Check if player hits ship 1 or ship 2
-    if computerCoordGrid[random_num3][random_num4] == "x":
-        shipAnimation_hit()
-        print("You sunk the Computer's ship! You WIN!")
-        player_win()
-        t=10
+def Turn_system(turn,playerDisplayGrid, playerCoordGrid, ship1_name, ship2_name, computerDisplayGrid, computerCoordGrid, row1, col1, row2, col2, row3, col3, row4, col4, random_num3, random_num4, row_list, col_list):
+    print("Objective:\nSink The Computer's Ship")
+    if turn == 0:
+        playerBattleGrid(playerDisplayGrid, row_list, col_list)
+        turn=0
+        Shoot_Guess(playerDisplayGrid, playerCoordGrid,row_list, col_list,turn)
+
+        if playerCoordGrid[row1][col1] == playerCoordGrid[row2][col2] == playerCoordGrid[row3][col3] == playerCoordGrid[row4][col4] == "x":
+            print("The computer sunk both your ships! Better luck next time.")
+            computer_win()
+        t=5
         while t > 0:
-            print(f"{Fore.WHITE}Back to menu in {t % 60:02}", end=" seconds.\r")  # display minutes and seconds
+            print(f"{Fore.WHITE}Player moves in {t % 60:02}", end=" seconds.\r")  # display minutes and seconds
             time.sleep(1)  # wait for 1 second
             t -= 1
-        game()
-        
-        
-    os.system('cls' if os.name == 'nt' else 'clear')
+            
+    
+        # Check if player hits ship 1 or ship 2
+        print("Move Log:")
+        if playerCoordGrid[row1][col1] == "x":
+            print(f"The computer hit your ship, {ship1_name}!")
+        if playerCoordGrid[row2][col2] == "x":
+            print(f"The computer hit your ship, {ship1_name}!")
+        if playerCoordGrid[row3][col3] == "x":
+            print(f"The computer hit your ship, {ship2_name}!")
+        if playerCoordGrid[row4][col4] == "x":
+            print(f"The computer hit your ship, {ship2_name}!")
 
-# Gianna
-# Function for manual ship placement
-def computer_turn(playerDisplayGrid, playerCoordGrid, ship1_name, ship2_name, row1, col1, row2, col2, row3, col3, row4, col4, random_num3, random_num4, row_list, col_list):
+        if playerCoordGrid[row1][col1] == playerCoordGrid[row2][col2] == "x":
+            print(f"The computer sunk your ship, {ship1_name}!")
+        if playerCoordGrid[row3][col3] == playerCoordGrid[row4][col4] == "x":
+            print(f"The computer sunk your ship, {ship2_name}!")
+        else:
+            print("The computer missed!")
+        t = 3
+        print(f"{Fore.WHITE}User turn in{t % 60:02}", end=" seconds.\r")  # display minutes and seconds
+        time.sleep(1)  # wait for 1 second
 
-    print("Computer Objective:\nSink The User's Ship In Five Turns")
-    # printship(s)
 
-    playerBattleGrid(playerDisplayGrid, row_list, col_list)
-    turn=0
-    Shoot_Guess(playerDisplayGrid, playerCoordGrid,row_list, col_list,turn)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    
+    else:
+        computerBattleGrid(computerDisplayGrid, row_list, col_list)
+        turn = 1
+        Shoot_Guess(computerDisplayGrid, computerCoordGrid, col_list, row_list,turn)
+        if computerCoordGrid[random_num3][random_num4] != "x":
+            shipAnimation_miss()
 
-    if playerCoordGrid[row1][col1] == playerCoordGrid[row2][col2] == playerCoordGrid[row3][col3] == playerCoordGrid[row4][col4] == "x":
-        print("The computer sunk both your ships! Better luck next time.")
-        computer_win()
-        t=10
-        while t > 0:
-            print(f"{Fore.WHITE}Back to menu in {t % 60:02}", end=" seconds.\r")  # display minutes and seconds
-            time.sleep(1)  # wait for 1 second
-            t -= 1
-        game()
-
+        # Check if player hits ship 1 or ship 2
+        if computerCoordGrid[random_num3][random_num4] == "x":
+            shipAnimation_hit()
+            print("You sunk the Computer's ship! You WIN!")
+            player_win()
+            t=10
+            while t > 0:
+                print(f"{Fore.WHITE}Back to menu in {t % 60:02}", end=" seconds.\r")  # display minutes and seconds
+                time.sleep(1)  # wait for 1 second
+                t -= 1
+            game()
+            
+            
+            os.system('cls' if os.name == 'nt' else 'clear')
 
 
     # Check if player hits ship 1 or ship 2
@@ -891,9 +908,10 @@ def game():
 
     # Alternates turns between the computer and the player
     while True:
-        computer_turn(playerDisplayGrid, playerCoordGrid, ship1_name, ship2_name, row1, col1, row2, col2, row3, col3, row4, col4, random_num3, random_num4, row_list, col_list)
-        player_turn(computerDisplayGrid, computerCoordGrid, row1, col1, row2, col2, row3, col3, row4, col4, random_num3, random_num4, row_list, col_list)
-
+        turn=0
+        Turn_system(turn,playerDisplayGrid, playerCoordGrid, ship1_name, ship2_name, computerDisplayGrid, computerCoordGrid, row1, col1, row2, col2, row3, col3, row4, col4, random_num3, random_num4, row_list, col_list)  
+        turn=1
+        Turn_system(turn,playerDisplayGrid, playerCoordGrid, ship1_name, ship2_name, computerDisplayGrid, computerCoordGrid, row1, col1, row2, col2, row3, col3, row4, col4, random_num3, random_num4, row_list, col_list)  
         
         
         
